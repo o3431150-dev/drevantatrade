b28d4c433c6d9bef21ec8f0b8f50fde48b7674d8
egla24330 <egla24330@gmail.com>
2026-04-23 17:51:09 -0300

    fix error

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
 
 /* ---------- CORS ---------- */
 const allowedOrigins = [
-  "https://trading-appv1.onrender.com"
+  "https://trading-appv1.onrender.com",
   "http://localhost:3000",
   "http://localhost:5173",
   "https://trading-app-fdzj.onrender.com",
